570908494ccd91b3abc163b12845ccac1a3f5434
TiagoGarcia712 <101438324+TiagoGarcia712@users.noreply.github.com>
2022-04-19 11:36:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 app.get('/', (req, res) => {
     res.status(200).send({ message: 'Olá mundo estou funcionando!' });
 })
+app.get('/Olá', (req, res) => {
+    res.status(200).send({ message: 'Oi sou a rota Olá' });
+})
 
 app.listen(3001, ()=>{
     console.log('Api rodando na porta 3001');
